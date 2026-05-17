7236f635ddd12c31c70ddb4b49562c4f911275d1
Ario <Ariobashiri@gmail.com>
2026-05-17 19:05:46 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ function App(){
     return(
         <>
             <User username="Ario" image={ProfilePic} description="I'm a cool gamer I love programming."/>
-            <User username="Aida" image={ProfilePic} description="I'm a cool sister. I'm the gamer's sister."/>
-            <User username="Leila" image={ProfilePic} description="I freaking made the freaking cool gamer."/>
-            <User username="Daruosh" image={ProfilePic} description="I'm the reason the freaking cool gamer exists."/>
 
         </>
         
